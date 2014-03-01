bbb51fdf67a2b5380579232af6dc4eafab7c7a79
cerkiner <cerkiner@gmail.com>
2014-03-01 06:34:08 +0200

    beta

--- src/com/cerkiner/esogu/ieee/MyPhoneGapActivity.java
+++ src/com/cerkiner/esogu/ieee/MyPhoneGapActivity.java
@@ -1,6 +1,9 @@
 package com.cerkiner.esogu.ieee;
 
 import org.apache.cordova.DroidGap;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MyPhoneGapActivity extends DroidGap {
@@ -8,5 +11,10 @@ public class MyPhoneGapActivity extends DroidGap {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		super.loadUrl("file:///android_asset/www/index.html");
+	}
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+	    super.onConfigurationChanged(newConfig);
+	    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 	}
 }
